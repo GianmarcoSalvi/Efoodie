327b42e6ac03c04b630befcb8135338ab51051c5
Gianmarco Salvi <gianmarco10.salvi@live.it>
2022-06-22 14:23:57 -0300

    Fragments and Activities open correctly from the drawer menu.

--- app/src/main/java/com/example/efoodie/MainActivity.java
+++ app/src/main/java/com/example/efoodie/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_explore, R.id.nav_favorites, R.id.nav_upload)
+                R.id.nav_explore, R.id.nav_favorites, R.id.nav_upload, R.id.nav_profile)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
