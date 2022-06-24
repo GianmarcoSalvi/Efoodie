260882332c8e437b5fa270603921546644c5a82e
Gianmarco Salvi <gianmarco10.salvi@live.it>
2022-06-23 23:41:19 -0300

    Explore fragment: - Added "ordenar por" with all possible sorting options General: - Hiding keyboard when drawer menu opens - Hiding text selector when drawer menu opens

--- app/src/main/java/com/example/efoodie/MainActivity.java
+++ app/src/main/java/com/example/efoodie/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.efoodie;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,6 +20,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.efoodie.databinding.ActivityMainBinding;
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.util.concurrent.ServiceManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +47,28 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+                closeKeyboard_clearFocus();
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                closeKeyboard_clearFocus();
+            }
+        });
     }
 
     @Override
@@ -49,4 +77,18 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+    // Used to clear focus (for ex. text highlighted) and hide keyboard when drawer menu opens
+    private void closeKeyboard_clearFocus(){
+        View view = this.getCurrentFocus();
+        if(view != null){
+            InputMethodManager inputMethodManager = (InputMethodManager)
+                    getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            view.clearFocus();
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/efoodie/ui/explore/ExploreFragment.java
+++ app/src/main/java/com/example/efoodie/ui/explore/ExploreFragment.java
@@ -1,20 +1,34 @@
 package com.example.efoodie.ui.explore;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.efoodie.MainActivity;
+import com.example.efoodie.R;
 import com.example.efoodie.databinding.FragmentExploreBinding;
+
+import java.lang.reflect.Array;
 
 public class ExploreFragment extends Fragment {
 
     private FragmentExploreBinding binding;
+    ArrayAdapter arrayAdapter;
+    AutoCompleteTextView autoCompleteTextView;
+    Spinner spinnerTextSize;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,8 +36,43 @@ public class ExploreFragment extends Fragment {
         binding = FragmentExploreBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // Setup listener over filters button in the search bar
+        RelativeLayout filters_btn = (RelativeLayout) root.findViewById(R.id.filter_button);
+        filters_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(), FiltersActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
+        // Setup sorting (ordenar por)
+        String[] dropdown_menu_items = getResources().getStringArray(R.array.sort_array);
+        autoCompleteTextView = root.findViewById(R.id.autoCompleteTextView);
+        arrayAdapter = new ArrayAdapter(requireContext(),R.layout.dropdown_item, dropdown_menu_items);
+        binding.autoCompleteTextView.setAdapter(arrayAdapter);
+
+        binding.autoCompleteTextView.setDropDownBackgroundDrawable(
+                ResourcesCompat.getDrawable(
+                        getResources(),
+                        R.drawable.filter_spinner_bg,
+                        null
+                )
+        );
+
+
+        // TODO - Setup listener for sorting (backend)
+        /* autoCompleteTextView.setOnClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick()
+        } */
+
+
         return root;
+
     }
+
+
 
 }
 
