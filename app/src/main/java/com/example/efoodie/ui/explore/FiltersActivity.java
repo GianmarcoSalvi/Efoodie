1c3fdd77b713680d3ffa8737b812b78c30e59d21
Gianmarco Salvi <gianmarco10.salvi@live.it>
2022-06-23 23:42:06 -0300

    Explore fragment: - Added "ordenar por" with all possible sorting options General: - Hiding keyboard when drawer menu opens - Hiding text selector when drawer menu opens

---
+++ app/src/main/java/com/example/efoodie/ui/explore/FiltersActivity.java
@@ -0,0 +1,16 @@
+package com.example.efoodie.ui.explore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.efoodie.R;
+
+public class FiltersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filters);
+    }
+}
